4f05bf8210983aeba2c0dddfc75593d2431cc61b
Reshma Hussain <reshnaz91@gmail.com>
2018-02-05 01:30:53 +0530

    Commit

---
+++ src/app/routes/person-routes.routes.ts
@@ -0,0 +1,9 @@
+import { RouterModule, Routes } from '@angular/router';
+/*
+Here we configure routing and router outlet.
+*/
+const routes: Routes = [
+];
+
+// A const that holds all our routing configuration. This is globally imported in @NgModule.
+export const personRouting = RouterModule.forRoot(routes);
